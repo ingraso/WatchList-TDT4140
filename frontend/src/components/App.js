faa55847050511121709f594b422bc68570b59d7
Ingrid Amalie <iasolbjorg@gmail.com>
2020-02-26 14:37:26 +0100

    Add link to show add movies form

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,43 +2,50 @@ import React from "react";
 //import NavBar from "./navbar";
 import Profile from "./users/profile";
 import Login from "./users/login";
+import Select from "./movies/index.js";
 import MoviesDisplay from "./movies/moviesDisplay";
-import landingp from "./pages/landingP";
 import "./app.css";
-import { BrowserRouter,Switch, Route,Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return <React.Fragment>
-  <BrowserRouter>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/movies">Movies</Link>
-          </li>
-        </ul>
-      </nav>
-      {/* A <Switch> looks through its children <Route>s and
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/showMovies">Movies</Link>
+              </li>
+              <li>
+                <Link to="/addMovies">Add movies and series</Link>
+              </li>
+            </ul>
+          </nav>
+          {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
-      <Switch>
-        <Route path="/movies">
-        <MoviesDisplay></MoviesDisplay>
-        </Route>
-        <Route path="/create">
-      <Profile></Profile>
-        </Route>
-        <Route path="/">
-      <Login></Login>
-      <landingp></landingp>
-        <Link to="/create">Create</Link>
-        </Route>
-      </Switch>
-    </div>
-  </BrowserRouter>
-    </React.Fragment>;
+          <Switch>
+            <Route path="/showMovies">
+              <MoviesDisplay></MoviesDisplay>
+            </Route>
+            <Route path="/addMovies">
+              <Select></Select>
+            </Route>
+            <Route path="/create">
+              <Profile></Profile>
+            </Route>
+            <Route path="/">
+              <Login></Login>
+              <Link to="/create">Create</Link>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
 
 export default App;

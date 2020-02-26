ae75f94e128952193457bd66eb9e5100df462edd
Ingrid Amalie <iasolbjorg@gmail.com>
2020-02-26 10:30:14 +0100

    Fix merge conflicts

--- frontend/src/components/movies/moviesDisplay.jsx
+++ frontend/src/components/movies/moviesDisplay.jsx
@@ -33,48 +33,36 @@ class MoviesDisplay extends Component {
       .then(() => {
         this.setState({ list: result });
         displaymovies = this.state.list.map((movie, index) => (
-          <div>
-            <div className="movie" key={index + movie[0].title}>
+          <div className="movie" key={index + movie[0].title}>
+            <div className="movieHeader">
               <h1 className="title">{movie[0].title}</h1>
-              <div>
+            </div>
+            <div className="movieInfoContainer">
+              <div className="movieImage">
                 <img
                   width="200"
                   alt={`The movie titled: ${movie[0].title}`}
                   src={movie[0].image}
                 />
+              </div>
 
-                <p>Regisert av: {movie[0].director}</p>
+              <div className="movieInformation">
                 <p>
-                  Description placeholder text: Lorem Ipsum is simply dummy text
-                  of the printing and typesetting industry. Lorem Ipsum has been
-                  the industry's standard dummy text ever since the 1500s, when
-                  an unknown printer took a galley of type and scrambled it to
-                  make a type specimen book. It has survived not only five
-                  centuries, but also the leap into electronic typesetting,
-                  remaining essentially unchanged. It was popularised in the
-                  1960s with the release of Letraset sheets containing Lorem
-                  Ipsum passages, and more recently with desktop publishing
-                  software like Aldus PageMaker including versions of Lorem
-                  Ipsum{" "}
+                  <b>Directed by:</b> {movie[0].director} <br></br>
+                  <b>Duration:</b> {movie[0].duration} <br></br>
+                  <b>Score:</b> {movie[0].score} <br></br>
+                  <p>
+                    <b>Description placeholder text:</b> Lorem Ipsum is simply
+                    dummy text of the printing and typesetting industry. Lorem
+                    Ipsum has been the industry's standard dummy text ever since
+                    the 1500s, when an unknown printer took a galley of type and
+                    scrambled it to make a type specimen book. It has survived
+                    not only five centuries, but also the leap into electronic
+                    typesetting, remaining essentially unchanged. It was
+                    popularised in the 1960s with the{" "}
+                  </p>
                 </p>
               </div>
-            </div>
-            <div className="movieInformation">
-              <p>
-                <b>Directed by:</b> {movie[0].director} <br></br>
-                <b>Duration:</b> {movie[0].duration} <br></br>
-                <b>Score:</b> {movie[0].score} <br></br>
-                <p>
-                  <b>Description placeholder text:</b> Lorem Ipsum is simply
-                  dummy text of the printing and typesetting industry. Lorem
-                  Ipsum has been the industry's standard dummy text ever since
-                  the 1500s, when an unknown printer took a galley of type and
-                  scrambled it to make a type specimen book. It has survived not
-                  only five centuries, but also the leap into electronic
-                  typesetting, remaining essentially unchanged. It was
-                  popularised in the 1960s with the{" "}
-                </p>
-              </p>
             </div>
           </div>
         ));

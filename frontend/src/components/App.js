0ded6437645d4a85c0d445ce98e775d6344b253b
Henrik Hodnefjeld <32102175+henrihod@users.noreply.github.com>
2020-02-26 21:45:02 +0100

    Displaying name of logging in user
    Also diferentiates between admin and normal user based on who is signed in

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -12,21 +12,6 @@ function App() {
     <React.Fragment>
       <BrowserRouter>
         <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/showMovies">Movies</Link>
-              </li>
-              <li>
-                <Link to="/addMovies">Add movies and series</Link>
-              </li>
-            </ul>
-          </nav>
-          {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
           <Switch>
             <Route path="/showMovies">
               <MoviesDisplay></MoviesDisplay>
@@ -39,7 +24,7 @@ function App() {
             </Route>
             <Route path="/">
               <Login></Login>
-              <Link to="/create">Create</Link>
+
             </Route>
           </Switch>
         </div>

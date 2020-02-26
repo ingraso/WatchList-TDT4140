ca1a0655a7f297debdbf4c9666ea8590c8cab30e
Ingrid Amalie <iasolbjorg@gmail.com>
2020-02-26 09:46:46 +0100

    Add example styling for movies

--- frontend/src/components/movies/moviesDisplay.jsx
+++ frontend/src/components/movies/moviesDisplay.jsx
@@ -1,72 +1,99 @@
 import React, { Component } from "react";
-import { data } from '../../Backend/getMovies.js';
-import $ from 'jquery';
+import { data } from "../../Backend/getMovies.js";
+import $ from "jquery";
 import "./movie.css";
- var displaymovies
+
+var displaymovies;
+// onClick={deleteMovie(movieID)}
+
 class MoviesDisplay extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list:"a",
+      list: "a"
     };
   }
 
-    fetchData = async () => {
-      var result
-      var settings = {
-        url:
-          "https://watchlistas.firebaseio.com/entertainment/movie.json?auth=qWIkHwOFG3EpS9gYCNP50tndNOFBS57ta41Rcy1f",
-        method: "GET",
-        timeout: 0,
-        headers: {
-          "Access-Control-Allow-Credentials": "true"
-        }
-      };
-       const fetchedData =  $.ajax(settings).then(function(response) {
-     result = Object.keys(response).map(function(key) {
-      return [response[key]];
-    });
+  fetchData = async () => {
+    var result;
+    var settings = {
+      url:
+        "https://watchlistas.firebaseio.com/entertainment/movie.json?auth=qWIkHwOFG3EpS9gYCNP50tndNOFBS57ta41Rcy1f",
+      method: "GET",
+      timeout: 0,
+      headers: {
+        "Access-Control-Allow-Credentials": "true"
+      }
+    };
+    const fetchedData = $.ajax(settings)
+      .then(function(response) {
+        result = Object.keys(response).map(function(key) {
+          return [response[key]];
+        });
+      })
+      .then(() => {
+        this.setState({ list: result });
+        displaymovies = this.state.list.map((movie, index) => (
+          <div>
+            <div className="movie" key={index + movie[0].title}>
+              <h1 className="title">{movie[0].title}</h1>
+              <div>
+                <img
+                  width="200"
+                  alt={`The movie titled: ${movie[0].title}`}
+                  src={movie[0].image}
+                />
 
-}).then(() => {
-    this.setState({ list: result });
-  displaymovies = this.state.list.map((movie, index) =>  (
-    <div className="movie" key={index+movie[0].title}>
-  <h1 className="title">{movie[0].title}</h1>
-  <div>
-    <img
-      width="200"
-      alt={`The movie titled: ${movie[0].title}`}
-      src={movie[0].image}
-    />
-  
-  <p>Regisert av: {movie[0].director}</p>
-  <p>Description placeholder text: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </p>
-
-    </div>
-    </div>
-))
-
-    this.setState({ list: result });
-    console.log(this.state.list)
-})
-}
-    componentDidMount(){
-        this.fetchData();
-
-    }
-  render() {
-
-    return (
-<div>
-      <div className="moviesContainer">
-      {displaymovies}
+                <p>Regisert av: {movie[0].director}</p>
+                <p>
+                  Description placeholder text: Lorem Ipsum is simply dummy text
+                  of the printing and typesetting industry. Lorem Ipsum has been
+                  the industry's standard dummy text ever since the 1500s, when
+                  an unknown printer took a galley of type and scrambled it to
+                  make a type specimen book. It has survived not only five
+                  centuries, but also the leap into electronic typesetting,
+                  remaining essentially unchanged. It was popularised in the
+                  1960s with the release of Letraset sheets containing Lorem
+                  Ipsum passages, and more recently with desktop publishing
+                  software like Aldus PageMaker including versions of Lorem
+                  Ipsum{" "}
+                </p>
+              </div>
+            </div>
+            <div className="movieInformation">
+              <p>
+                <b>Directed by:</b> {movie[0].director} <br></br>
+                <b>Duration:</b> {movie[0].duration} <br></br>
+                <b>Score:</b> {movie[0].score} <br></br>
+                <p>
+                  <b>Description placeholder text:</b> Lorem Ipsum is simply
+                  dummy text of the printing and typesetting industry. Lorem
+                  Ipsum has been the industry's standard dummy text ever since
+                  the 1500s, when an unknown printer took a galley of type and
+                  scrambled it to make a type specimen book. It has survived not
+                  only five centuries, but also the leap into electronic
+                  typesetting, remaining essentially unchanged. It was
+                  popularised in the 1960s with the{" "}
+                </p>
+              </p>
+            </div>
           </div>
+        ));
+
+        this.setState({ list: result });
+        console.log(this.state.list);
+      });
+  };
+  componentDidMount() {
+    this.fetchData();
+  }
+  render() {
+    return (
+      <div>
+        <div className="moviesContainer">{displaymovies}</div>
       </div>
     );
   }
-
-
-
 }
 
 export default MoviesDisplay;
--- frontend/src/components/users/login.jsx
+++ frontend/src/components/users/login.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import $ from 'jquery';
-import {getuser} from "./getuser";
-import { Redirect } from 'react-router';
-import createHistory from 'history/createBrowserHistory'
-import "./landingpage.css"; 
-
+import $ from "jquery";
+import { getuser } from "./getuser";
+import { Redirect } from "react-router";
+import createHistory from "history/createBrowserHistory";
+import "./landingpage.css";
 
 class Login extends Component {
   constructor(props) {
@@ -25,89 +24,82 @@ class Login extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-this.fetchUsers();
-
-};
-fetchUsers = async () => {
-let response = await getuser();
-console.log(this.state.username + "  " + this.state.password)
-console.log(response)
-var result = Object.keys(response).map(function(key) {
-  return [response[key]];
-})
-result.map((result, index) => {
-console.log(result[0].username)
-console.log(result[0].password)
-console.log(this.state.username)
-if (result[0].username == this.state.username  && result[0].password == this.state.password) {
-  console.log("logged in")
-  const history = createHistory()
-  history.push('/movies')
-  window.location.reload();
-}
-})
-}
+    this.fetchUsers();
+  };
+  fetchUsers = async () => {
+    let response = await getuser();
+    console.log(this.state.username + "  " + this.state.password);
+    console.log(response);
+    var result = Object.keys(response).map(function(key) {
+      return [response[key]];
+    });
+    result.map((result, index) => {
+      console.log(result[0].username);
+      console.log(result[0].password);
+      console.log(this.state.username);
+      if (
+        result[0].username === this.state.username &&
+        result[0].password === this.state.password
+      ) {
+        console.log("logged in");
+        const history = createHistory();
+        history.push("/movies");
+        window.location.reload();
+      }
+    });
+  };
   render() {
-
     return (
-    <div>
-          <div> 
+      <div>
+        <div>
+          <div className="app-container">
+            <div className="nav">
+              <div className="nav-left">
+                <input className="nav-search-input" placeholder="Search" />
+                <button className="nav-search-button">Search</button>
+              </div>
 
-<div className="app-container">
+              <div className="nav-center">
+                <div className="nav-logo">
+                  <h2> WatchList</h2>
+                </div>
+              </div>
 
-<div className="nav">
+              <div className="nav-right">
+                <div className="nav-button">Sign up</div>
+                <div className="nav-button">Log in</div>
+              </div>
+            </div>
 
-<div className="nav-left">
-  <input
-    className="nav-search-input"
-    placeholder="Search"
-  />
-  <button className="nav-search-button">Search</button>
-</div>
+            <div className="movies">
+              <div className="movies-right">
+                {" "}
+                <h5>This weeks: </h5>
+              </div>
+            </div>
+          </div>
 
-<div className="nav-center"> 
-    <div className="nav-logo"><h2> WatchList</h2></div>
-      </div>
-
-<div className="nav-right">
-  <div className="nav-button">Sign up</div>
-  <div className="nav-button">Log in</div>
-</div>
-
-</div>
-
-
-<div className="movies">
-
-<div className="movies-right"> <h5>This weeks: </h5></div>
-
-</div>   
-
-</div>
-
-
-<div>
-
-
-</div>
-</div>
+          <div></div>
+        </div>
         <form onSubmit={this.handleSubmit}>
           <div className="form-row">
             <div className="col">
+              Username:
               <input
                 type="text"
                 className="form-control"
-                placeholder="username"
+                placeholder="Your username"
                 value={this.state.username}
                 onChange={this.handleInputChange}
                 name="username"
               />
             </div>
             <div className="col">
+              Password:
               <input
                 type="password"
                 className="form-control"
-                placeholder="password"
+                placeholder="Your password"
                 value={this.state.password}
                 onChange={this.handleInputChange}
                 name="password"
@@ -117,17 +109,12 @@ if (result[0].username == this.state.username  && result[0].password == this.sta
 
           <button type="submit" className="btn btn-warning">
             {" "}
-            Loggin{" "}
+            Login{" "}
           </button>
         </form>
-
-    </div>
-
+      </div>
     );
   }
-
-
-
 }
 
 export default Login;
